d1441b450103a2c08c24c0d0326602c6bcb187d5
Pavan134 <73532489+Pavan134@users.noreply.github.com>
2021-08-13 11:36:54 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,6 @@
 package hello;
 
-public class Greeter {
   public String sayHello() {
     return "Hello world- This is N V PAVAN your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
-}
+
