6e9b0e55af3ab8b8a19046c8bb03b8eff55ac85d
Pavan134 <73532489+Pavan134@users.noreply.github.com>
2021-09-02 00:03:14 +0530

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
+class GreeterTest{
   private Greeter greeter = new Greeter();
 
   @Test
   public void greeterSaysHello() {
     assertThat(greeter.sayHello(), containsString("Bye"));
   }
-
+}
 
